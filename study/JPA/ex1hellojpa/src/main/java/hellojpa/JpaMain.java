32b3916b274358b0c333aca69132da7c214ae24b
hyun <jhyun_yi@naver.com>
2022-03-08 22:46:59 +0900

    jpa 수정, jpql 소개

--- study/JPA/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ study/JPA/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,13 +15,18 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+            Member findMember = em.find(Member.class, 1L);
 
-            em.persist(member);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
 
-            tx.commit();
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            findMember.setName("HelloJPA");
+
+            tx.commit(); //트랜잭선 직전에 변경되는 사항있는지 확인하고 쿼리 생성후 날린후 commit(수정)
         }catch (Exception e){
             tx.rollback();
         } finally {
